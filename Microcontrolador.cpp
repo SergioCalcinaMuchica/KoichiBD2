70e96cbbcde2d2a058189286471dd3820e14ea5a
SergioCalcinaMuchica <scalcinamu@unsa.edu.pe>
2025-06-04 02:02:55 -0500

    Disco como dato de Microcontrolador

--- Microcontrolador.cpp
+++ Microcontrolador.cpp
@@ -1,17 +1,18 @@
 #include "MicroControlador.h"
 #include "Disco.cpp"
 
-MicroControlador::MicroControlador() {
+MicroControlador::MicroControlador(Disco *d) {
+    disco = d;
     for (int i = 0; i < 4; i++) {
         ruta[i] = 0;
     }
 }
 
-void MicroControlador::ObtenerRuta(int LBA, Disco &d) {
-    ruta[0] = LBA / (2*d.pistas*d.sectores); // Plato ..sectoresxplato
-    LBA = LBA%(2*d.pistas*d.sectores);
-    ruta[1] = LBA / (d.pistas*d.sectores); // Superficie ..sectoresxsuperficie
-    LBA = LBA%(d.pistas*d.sectores);
-    ruta[2] = LBA/d.sectores; // Pista
-    ruta[3] = LBA % d.sectores; // Sector
+void MicroControlador::ObtenerRuta(int LBA) {
+    ruta[0] = LBA / (2*disco->pistas*disco->sectores); // Plato ..sectoresxplato
+    LBA = LBA%(2*disco->pistas*disco->sectores);
+    ruta[1] = LBA / (disco->pistas*disco->sectores); // Superficie ..sectoresxsuperficie
+    LBA = LBA%(disco->pistas*disco->sectores);
+    ruta[2] = LBA/disco->sectores; // Pista
+    ruta[3] = LBA % disco->sectores; // Sector
 }
